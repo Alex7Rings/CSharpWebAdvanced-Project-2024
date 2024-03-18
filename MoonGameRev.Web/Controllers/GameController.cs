2bae7439443f2c6d0f97532cbe3617933dd4aa80
aka_alex77 <aka_alex77@DESKTOP-448S690>
2024-03-18 17:35:49 +0200

    Added Controllers

---
+++ MoonGameRev.Web/Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoonGameRev.Web.Controllers
+{
+    [Authorize]
+    public class GameController : Controller
+    {
+        [AllowAnonymous]
+        public async Task<IActionResult> Games()
+        {
+            return View();
+        }
+    }
+}
---
+++ MoonGameRev.Web/Controllers/ReviewController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoonGameRev.Web.Controllers
+{
+    [Authorize]
+    public class ReviewController : Controller
+    {
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+    }
+}
